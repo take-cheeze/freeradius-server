900dd0a71f4533d2ca54cebc2e0dbfb9698d83fb
Herwin Weststrate <herwin@snt.utwente.nl>
2016-11-26 11:35:10 +0100

    Updated example.rb
    This one works with the current code

--- src/modules/rlm_mruby/example.rb
+++ src/modules/rlm_mruby/example.rb
@@ -1,25 +1,22 @@
 #This is example radius.rb script
-module Radiusd
-    def Radiusd.instantiate(arg)
-        radlog(L_DBG,"[ruby]Running ruby instantiate")
-        p arg
-        return Radiusd::RLM_MODULE_OK
-    end
-    def Radiusd.authenticate(arg)
-        radlog(L_DBG,"[ruby]Running ruby authenticate")
-        p arg
-        return Radiusd::RLM_MODULE_NOOP
-    end
-    def Radiusd.authorize(arg)
-        radlog(L_DBG,"[ruby]Running ruby authorize")
-        p arg
-        #Here we return Cleartext-Password, which could have been retrieved from DB.
-        return [Radiusd::RLM_MODULE_UPDATED, [],[["Cleartext-Password","pass"]]]
-    end
-    def Radiusd.accounting(arg)
-        radlog(L_DBG,"[ruby]Running ruby accounting")
-        p arg
-        return Radiusd::RLM_MODULE_NOOP
-    end
-
+def instantiate()
+    Radiusd.radlog(Radiusd::L_DBG, "[mruby]Running ruby instantiate")
+    puts "[mruby]Running mruby instantiate"
+    return Radiusd::RLM_MODULE_OK
+end
+def authenticate(arg)
+    Radiusd.radlog(Radiusd::L_DBG, "[mruby]Running ruby authenticate")
+    return Radiusd::RLM_MODULE_NOOP
+end
+def authorize(arg)
+    Radiusd.radlog(Radiusd::L_ERR, "[mruby]Running ruby authorize")
+		Radiusd.radlog(Radiusd::L_WARN, "Authorize: #{arg.inspect}(#{arg.class})")
+    Radiusd.radlog(Radiusd::L_WARN, "Authorize: #{arg.request.inspect}(#{arg.request.class})")
+    #Here we return Cleartext-Password, which could have been retrieved from DB.
+    return [Radiusd::RLM_MODULE_UPDATED, [],[["Cleartext-Password", "hello"]]]
+end
+def accounting(arg)
+    Radiusd.radlog(Radiusd::L_DBG, "[mruby]Running ruby accounting")
+    p arg
+    return Radiusd::RLM_MODULE_NOOP
 end
